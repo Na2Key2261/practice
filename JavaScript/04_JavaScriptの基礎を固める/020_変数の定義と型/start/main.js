40663c62fc739b83cbab838424a6fee6ec902fa5
Na2Key2261 <natsuki.97285@gmail.com>
2024-03-07 01:20:56 +0000

    add react_03

---
+++ JavaScript/04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,10 @@
+let name = 'Tom';
+name = 'Tim';
+console.log('hello ' + name);
+
+let variable = 'str';
+variable = 12;
+variable = false;
+variable = undefined;
+
+console.log(typeof variable);
---
+++ react/react-guide-material 2/03_js_basic/070_conditional_operator/start/main.js
@@ -0,0 +1,16 @@
+const a = 0;
+let resultA = a ? 10 : -10;
+
+// if(a) {
+//   resultA = "true";
+// } else {
+//   resultA = "false";
+// }
+
+console.log(resultA);
+
+function getResult() {
+  return a ? "true" : "false";
+}
+
+console.log(getResult());
