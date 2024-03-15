967798ecd907b9d1602ef4a68b2e8ccec866ab0a
Na2Key2261 <natsuki.97285@gmail.com>
2024-03-15 07:16:08 +0000

    add

---
+++ JavaScript/04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -0,0 +1,7 @@
+function hello(name, age) {
+    console.log('hello ' + name + age);
+    return name + age;
+}
+hello('Code Mafia', 10);
+const returnVal = hello('Code Mafia 2', 20);
+console.log(returnVal);
