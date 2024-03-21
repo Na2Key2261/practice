29e93db05f0f6ced40115b3aa966db46d6a8879d
Na2Key2261 <natsuki.97285@gmail.com>
2024-03-21 13:36:38 +0000

    add

--- JavaScript/04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
+++ JavaScript/04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
@@ -1,13 +1,15 @@
-function hello(callback, lastname) {
-    console.log(callback);
-    console.log('hello ' + callback(lastname));
+function doSomething(a, b, callback) {
+    const result = callback(a, b);
+    console.log(result);
 }
 
-function getName() {
-    return 'Code Mafia';
-}
-const getFirstName = function(name) {
-    return 'Code' + name;
+function multiply(a, b) {
+    return a * b;
 }
 
-hello(getFirstName, 'Mafia');
+function plus(a, b) {
+    return a + b;
+}
+
+doSomething(2, 2, multiply);
+doSomething(2, 3, plus);
