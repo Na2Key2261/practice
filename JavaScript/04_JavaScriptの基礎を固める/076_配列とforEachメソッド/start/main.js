de38bc9d09828c51f30954d47f1bf7871ef0f9e4
Na2Key2261 <natsuki.97285@gmail.com>
2024-03-23 10:26:45 +0000

    add

--- JavaScript/04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
+++ JavaScript/04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
@@ -4,4 +4,9 @@ arry.forEach(function(v, i, ary) {
     console.log(v);
 });
 
-arry.forEach(v => console.log(v));
+// arry.forEach(v => console.log(v));
+
+for(let i = 0; i < arry.length; i++) {
+    const v = arry[i];
+    console.log(v);
+}
